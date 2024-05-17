346b058e3924e5443e75676479ae1e85679acc6b
Gooogasol <egorshevchuk6a@gmail.com>
2024-05-17 20:44:53 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let width = window.innerWidth
+let git_hub = {    targets:'.github',
+    translateX: width/2-50,
+    rotate: 360,
+    duration: 1500,
+    delay: 3000,
+    easing: 'linear'}
+
+anime(git_hub)
